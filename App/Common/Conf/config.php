ffede7cb0464cb94b5069bf31637562c8ff1a2ce
LeonardLmytHome <13539792080@163.com>
2018-01-10 00:26:17 +0800

    尝试数据库链接

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -5,8 +5,8 @@ return array(
     'DB_TYPE'   => 'mysql', // 数据库类型
 
 
-    'DB_HOST'   => 'localhost', // 服务器地址
-    'DB_NAME'   => 'xueyuantest', // 数据库名
+    'DB_HOST'   => '120.79.6.64', // 服务器地址
+    'DB_NAME'   => 'xueyuan', // 数据库名
     'DB_USER'   => 'root', // 用户名
     'DB_PWD'    => 'root', // 密码
 
